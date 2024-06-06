c9ac2c0295620879a40b7c3ef093132013754dee
arturoburigo <burigoarturo3@gmail.com>
2024-06-05 23:12:09 -0300

    First commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import ErrorPage from './routes/ErrorPage.jsx'
+import Login from './routes/Login.jsx'
+import Home from './routes/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: '/',
+        element: <Login />
+      },
+      {
+        path: '/home',
+        element: <Home />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
