9abef594abb95f828cc3a1800cb1e7ccf0bfb59c
arturoburigo <burigoarturo3@gmail.com>
2024-06-12 19:15:16 -0300

    Header devleop

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,11 @@
+import Header from "../components/Header";
+import classes from "./Home.module.css"
+
 export default function Home(){
     return (
-        <div>
-            <h1>Home</h1>
+        <div className={classes}> 
+            <Header/>
+            
         </div>
     )
 }
