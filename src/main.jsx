0f9572d51f460063bbe5be81f4e34a0faf95c2c8
Gustavo Niero <141967161+gustavoniero@users.noreply.github.com>
2024-06-24 22:16:28 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Login from './routes/Login.jsx'
-import Home from './routes/Home.jsx'
-import SignUp from './routes/SignUp.jsx'
-import Profile from './routes/Profile.jsx'
-import EditCar from './routes/EditCar.jsx'
-import AddCar from "./routes/AddCar.jsx"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Login from './routes/Login.jsx';
+import Home from './routes/Home.jsx';
+import SignUp from './routes/SignUp.jsx';
+import Profile from './routes/Profile.jsx';
+import EditCar from './routes/EditCar.jsx';
+import AddCar from './routes/AddCar.jsx';
+import PrivateRoute from './routes/PrivateRoute.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App/>,
+    element: <App />,
     children: [
       {
         path: '/',
+        element: <PrivateRoute />
+      },
+      {
+        path: '/login',
         element: <Login />
       },
       {
         path: '/home',
-        element: <Home />
+        element: (
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        )
       },
       {
         path: '/signup',
@@ -28,22 +37,34 @@ const router = createBrowserRouter([
       },
       {
         path: '/profile',
-        element: <Profile />
+        element: (
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        )
       },
       {
-        path: "/edit/:id",
-        element: <EditCar/>
+        path: '/edit/:id',
+        element: (
+          <PrivateRoute>
+            <EditCar />
+          </PrivateRoute>
+        )
       },
       {
-        path: "/add",
-        element: <AddCar/>
+        path: '/add',
+        element: (
+          <PrivateRoute>
+            <AddCar />
+          </PrivateRoute>
+        )
       }
     ]
   }
-])
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
